758ebbf067320b20d3901cec33a707a7aa21aa63
Matthew-uk <esaduviedede@gmail.com>
2023-12-04 02:13:31 +0100

    Deposit system in place -v2.0

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,13 +6,14 @@ const multer = require("multer");
 const path = require("path");
 
 const getUsers = asyncHandler(async (req, res) => {
-  const { fullName, email, balance, referralCode } = await req.user;
+  const { fullName, email, balance, referralCode, _id } = await req.user;
   try {
     res.status(200).json({
       fullName,
       email,
       balance,
       referralCode,
+      id: _id,
     });
   } catch (error) {
     res.status(404).json(error.message);
